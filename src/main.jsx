92ea9910a4191f7205c16bb2dc92188504615424
ASD Admin <admin@advancedsteeldrafting.com>
2026-07-03 21:16:54 +1000

    Add src/main.jsx entry point and remove base path from vite config

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import RootApp from "../asd-app.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RootApp />
+  </React.StrictMode>
+);
